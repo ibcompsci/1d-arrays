13335a2b0d7bbaaf309044aa1e2d9dd2e9a8755b
Robert Griffiths <34591246+rbtgriffiths@users.noreply.github.com>
2019-09-20 09:05:34 -0500

    Create WhileLoops.java

---
+++ WhileLoops.java
@@ -0,0 +1,24 @@
+/**
+ * A program that demonstrates while loops.
+ */
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+        // while loop
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        // do/while loop
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        }
+        while (j < 10);
+    }
+}
