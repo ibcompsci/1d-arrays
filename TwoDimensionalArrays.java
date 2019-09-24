e86504076638031e4d5d52f6488227d16d86908c
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2019-09-23 23:35:55 -0500

    Update TwoDimensionalArrays.java

--- TwoDimensionalArrays.java
+++ TwoDimensionalArrays.java
@@ -26,7 +26,7 @@ public class TwoDimensionalArrays {
         }
         
         // Print the content of the array in table format
-		for (i = 0; i < 4; i++) {
+	for (i = 0; i < 4; i++) {
             for (j = 0; j < 5; j++) {
                 System.out.print(myArray[i][j] + "\t\t");
             }
